9e6d9d97a8408c584f06ee94d2dc0651a323f8e9
aae4 <as-prof@inbox.ru>
2013-03-24 19:18:30 +0300

    create Message bugfix

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  attr_accessible :body, :subject, :user_ids
+  attr_accessible :body, :subject, :user_ids, :service_id
   belongs_to :service
   has_and_belongs_to_many :users
 end

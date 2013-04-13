72989b42ff890e55f3335e62aedfbe957479442d
aabazov <aabazov@aabazov.(none)>
2013-04-13 11:59:39 +0400

    rails.calidations precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Sms::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( rails.validations.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true

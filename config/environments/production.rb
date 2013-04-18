dc37ee7c4d7c66a5d13c4e4adfb0db4cd21650cd
aabazov <aabazov@aabazov.(none)>
2013-04-18 21:35:02 +0400

    aplpication.js add default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Sms::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  #config.assets.compile = true
   #config.assets.precompile += %w( *.js *.css )
   #config.assets.precompile += %w( rails.validations.js )
 
